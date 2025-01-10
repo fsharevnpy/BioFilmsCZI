7f631b3e94bdf2e852ae3699d6c2865506628ece
nguyen.vo-xuan <nguyen.vo-xuan@banvien.com.vn>
2025-01-10 15:26:55 +0700

    Update function to remove background

--- Biofilms/src/intensity.py
+++ Biofilms/src/intensity.py
@@ -5,19 +5,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    # Read the .czi file
-    czi = CziFile('/home/nguyen/Biofilms/image/Image_21.czi')
-    image_array, _ = czi.read_image()  # image_array has shape (0, 0, 0, ch, z, h, w)
-
-    # Select the specific channel and Z slice
-    selected_channel = 0
-    selected_z = 0
-    origin_image = image_array[0, 0, 0, selected_channel, selected_z, :, :]
-    image = ((origin_image / image_array.max()) * (pow(2,8)-1)).astype(np.uint8)
-
-    ###############################################
-
+def extract_cell(image):
     background_mask = cv2.adaptiveThreshold(
         src=image,
         maxValue=255,
@@ -33,11 +21,27 @@ if __name__ == "__main__":
     # Extract the background
     background = cv2.bitwise_and(image, image, mask=background_mask)
     cell = cv2.bitwise_and(image, image, mask=cv2.bitwise_not(background_mask))
-    
+    return background, cell
+
+if __name__ == "__main__":
+    # Read the .czi file
+    czi = CziFile('/home/nguyen/BIO/BioFilmsCZI/Biofilms/image/Image_21.czi')
+    image_array, _ = czi.read_image()  # image_array has shape (0, 0, 0, ch, z, h, w)
+
+    # Select the specific channel and Z slice
+    selected_channel = 0
+    selected_z = 0
+    origin_image = image_array[0, 0, 0, selected_channel, selected_z, :, :]
+    image = ((origin_image / image_array.max()) * (pow(2,8)-1)).astype(np.uint8)
+
+    ###############################################
+
+    background, cell = extract_cell(image)
+
     # Display the results #########################
 
     # Count the number of zero pixels
-    print("intensity: ", np.sum(cell) - np.sum(background))
+    print("intensity: ", np.sum(cell[cell > 0]) - np.sum(background))
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
     plt.imshow(background, cmap='gray')
